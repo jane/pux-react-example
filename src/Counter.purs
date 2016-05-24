aa2256dffe0fc3e9ce2788b7cf5bd0d5aba11029
Michael Trotter <spicydonuts@gmail.com>
2016-05-24 17:10:48 -0600

    Comments in Counter.purs

--- src/Counter.purs
+++ src/Counter.purs
@@ -17,6 +17,7 @@ type ReactComponent a =
 
 foreign import puxProviderImpl :: forall a. ReactComponent a
 
+-- | This provider injects Pux context into the tree.  See `jsApp` below.
 puxProvider :: forall a b s. Channel b -> State s -> Array (Html a) -> Html a
 puxProvider chan state children =
   puxProviderImpl
@@ -27,6 +28,11 @@ puxProvider chan state children =
     ]
     children
 
+-- | This represents an arbitrarily large, existing React application.
+-- | Pux store context is provided to it via the puxProvider.
+-- | It'd be good to pull these two things into a single, simpler
+-- | helper function (ideally one that accepts the pre-Pux.fromReact
+-- | component).
 foreign import jsApp :: forall a. ReactComponent a
 
 view :: forall a s. Channel a -> State s -> Html a
